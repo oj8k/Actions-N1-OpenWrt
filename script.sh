5b21b3131548d894e8a7514cf36d7ca07ce2ea32
oj8k <37150741+oj8k@users.noreply.github.com>
2023-02-21 05:30:48 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 cd openwrt
 # Add luci-app-adguardhome
-git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package-temp/luci-app-adguardhome
-mv -f package-temp/luci-app-adguardhome package/lean/
-rm -rf package-temp
+#git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package-temp/luci-app-adguardhome
+#mv -f package-temp/luci-app-adguardhome package/lean/
+#rm -rf package-temp
 
 # Add luci-app-openclash
-git clone https://github.com/vernesong/OpenClash.git package-temp
-mv -f package-temp/luci-app-openclash package/lean/
-rm -rf package-temp
+#git clone https://github.com/vernesong/OpenClash.git package-temp
+#mv -f package-temp/luci-app-openclash package/lean/
+#rm -rf package-temp
+
 # Add luci-theme-opentomcat
 git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git theme-temp/luci-theme-opentomcat
 rm -rf theme-temp/luci-theme-opentomcat/LICENSE
@@ -17,6 +18,7 @@ mv -f theme-temp/luci-theme-opentomcat package/lean/
 rm -rf theme-temp
 default_theme='opentomcat'
 sed -i "s/bootstrap/$default_theme/g" feeds/luci/modules/luci-base/root/etc/config/luci
+
 # Add luci-app-vssr
 git clone https://github.com/jerrykuku/lua-maxminddb.git package-temp/lua-maxminddb
 git clone https://github.com/jerrykuku/luci-app-vssr.git package-temp/luci-app-vssr
@@ -24,4 +26,9 @@ git clone https://github.com/kenzok8/small.git package-temp/small
 cp -r package-temp/small/* package/lean/
 mv -f package-temp/lua-maxminddb package/lean/
 mv -f package-temp/luci-app-vssr package/lean/
+rm -rf package-temp
+
+# unblockneteasemusic
+git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package-temp/luci-app-unblockneteasemusic
+mv -f package-temp/luci-app-unblockneteasemusic package/lean/
 rm -rf package-temp
