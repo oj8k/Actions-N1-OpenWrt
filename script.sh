cf9e86c55ce3d178419183447a4ad82246d5c1bd
oj8k <37150741+oj8k@users.noreply.github.com>
2023-02-23 11:52:33 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -20,15 +20,14 @@ default_theme='opentomcat'
 sed -i "s/bootstrap/$default_theme/g" feeds/luci/modules/luci-base/root/etc/config/luci
 
 # Add luci-app-vssr
-git clone https://github.com/jerrykuku/lua-maxminddb.git package-temp/lua-maxminddb
-git clone https://github.com/jerrykuku/luci-app-vssr.git package-temp/luci-app-vssr
-git clone https://github.com/kenzok8/small.git package-temp/small
-cp -r package-temp/small/* package/lean/
-mv -f package-temp/lua-maxminddb package/lean/
-mv -f package-temp/luci-app-vssr package/lean/
-rm -rf package-temp
+#git clone https://github.com/jerrykuku/lua-maxminddb.git package-temp/lua-maxminddb
+#git clone https://github.com/jerrykuku/luci-app-vssr.git package-temp/luci-app-vssr
+#git clone https://github.com/kenzok8/small.git package-temp/small
+#cp -r package-temp/small/* package/lean/
+#mv -f package-temp/lua-maxminddb package/lean/
+#mv -f package-temp/luci-app-vssr package/lean/
+#rm -rf package-temp
 
-# unblockneteasemusic
-git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package-temp/luci-app-unblockneteasemusic
-mv -f package-temp/luci-app-unblockneteasemusic package/lean/
-rm -rf package-temp
+echo "src-git helloworld https://github.com/fw876/helloworld.git" >> ./feeds.conf.default
+echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> ./feeds.conf.default
+echo "src-git small https://github.com/kenzok8/small" >> ./feeds.conf.default
